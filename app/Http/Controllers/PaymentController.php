47be76e3074ed0dca7a364e713a59e17f991a96f
Manulakavishka <86448764+Manulakavishka@users.noreply.github.com>
2025-07-23 12:10:21 +0530

    order email when purchasing order

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\OnepayHelper;
+use App\Mail\OrderStatusUpdatedMail;
 use App\Models\CustomerOrder;
 use App\Models\CustomerOrderItems;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class PaymentController extends Controller
 {
@@ -39,6 +41,12 @@ class PaymentController extends Controller
                 'payment_method' => 'COD',
                 'payment_status' => 'Not Paid',
             ]);
+
+            // Send email to customer
+            if ($order->email) {
+                Mail::to($order->email)->send(new OrderStatusUpdatedMail($order, $order->status));
+            }
+
 
             return redirect()->route('order.thankyou', ['order_code' => $order_code])
                             ->with('success', 'Order confirmed successfully!');
@@ -160,6 +168,12 @@ class PaymentController extends Controller
         if ($order->payment_status != "Paid") {
             return redirect()->route('order.payment-fail')->with('error', 'Order not found.');
         }
+
+        // Send email to customer
+        if ($order->email) {
+            Mail::to($order->email)->send(new OrderStatusUpdatedMail($order, $order->status));
+        }
+
 
         $orderItems = CustomerOrderItems::where('order_code', $order_code)->get();
         return view('frontend.order_received', compact('order', 'orderItems'));
